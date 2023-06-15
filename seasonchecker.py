2fe6354e9b584c8b00765826d0c6bd2b90500ea7
Giorgi Beridze <gioberidze131@gmail.com>
2023-06-15 20:55:49 +0400

    add geo name of season

--- seasonchecker.py
+++ seasonchecker.py
@@ -2,13 +2,13 @@ def get_time_of_year(month):
     if month < 1 or month > 12:
         return "Invalid month number. Please enter a number from 1 to 12."
     elif month in [1, 2, 12]:
-        return "Winter"   
+        return "Winter" "zamtari"  
     elif month in [3, 4, 5]:
-        return "Spring"  
+        return "Spring"  "shemodgoma"
     elif month in [6, 7, 8]:
-        return "Summer"  
+        return "Summer"  "zafxuli"
     else:
-        return "Autumn"
+        return "Autumn" "gazafxuli"
 
 
 month_number = int(input("Enter a month number (1-12): "))
